1115129941795546664a82d0dc5fc4532ea31f51
Nils Sanderson <thebaldwan@gmail.com>
2017-07-12 01:42:46 +0100

    updated missing font file, icon and tried to get js going

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,13 @@
 $( document ).ready(function() {
-  console.log('hello');
-  $('.menu-icon').click(function() {
-    console.log('hello');
-    $(this).toggleClass('highlight');
+
+  $('.cta').click(function() {
+    var p = $( "p:first" );
+    $( "p:last" ).text( "scrollTop:" + p.scrollTop() );
   });
+
+  console.log('hello');
+  // $('.menu-icon').click(function() {
+  //   console.log('hello');
+  //   $(this).toggleClass('highlight');
+  // });
 });
